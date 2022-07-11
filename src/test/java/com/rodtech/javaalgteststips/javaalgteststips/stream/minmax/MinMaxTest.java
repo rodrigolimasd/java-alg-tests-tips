6de6bca18662aeedd06e35b2925d2aa57e09835c
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-07-11 18:31:45 -0300

    java 8 stream min test

---
+++ src/test/java/com/rodtech/javaalgteststips/javaalgteststips/stream/minmax/MinMaxTest.java
@@ -0,0 +1,31 @@
+package com.rodtech.javaalgteststips.javaalgteststips.stream.minmax;
+
+import com.rodtech.javaalgteststips.javaalgteststips.model.Person;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
+public class MinMaxTest {
+
+    @Test
+    public void whenListIsOfPersonObjectThenMinCanBeDoneUsingCustomComparatorThroughLambda() {
+        // given
+        Person alex = new Person("Alex", 23);
+        Person john = new Person("John", 40);
+        Person peter = new Person("Peter", 32);
+        List<Person> people = Arrays.asList(alex, john, peter);
+
+        // then
+        Person minByAge = people
+                .stream()
+                .min(Comparator.comparing(Person::getAge))
+                .orElseThrow(NoSuchElementException::new);
+
+        assertEquals("Should be Alex", alex, minByAge);
+    }
+}

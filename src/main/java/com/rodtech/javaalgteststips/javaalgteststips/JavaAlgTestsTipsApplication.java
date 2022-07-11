e5c6c1bbc48ffe121da72ea2115e218121bfe6e8
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-07-11 18:23:42 -0300

    Fisrt commit

---
+++ src/main/java/com/rodtech/javaalgteststips/javaalgteststips/JavaAlgTestsTipsApplication.java
@@ -0,0 +1,13 @@
+package com.rodtech.javaalgteststips.javaalgteststips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAlgTestsTipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAlgTestsTipsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodtech/javaalgteststips/javaalgteststips/JavaAlgTestsTipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodtech.javaalgteststips.javaalgteststips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAlgTestsTipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

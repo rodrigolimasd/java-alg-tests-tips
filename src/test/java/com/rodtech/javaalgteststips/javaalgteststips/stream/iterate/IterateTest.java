c9cb86514eab3eda13ac5e1895f37a3bec944b9c
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-07-13 12:29:17 -0300

    adjusts

--- src/test/java/com/rodtech/javaalgteststips/javaalgteststips/stream/iterate/IterateTest.java
+++ src/test/java/com/rodtech/javaalgteststips/javaalgteststips/stream/iterate/IterateTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.util.AssertionErrors.assertTrue;
 
 public class IterateTest {
@@ -16,11 +17,7 @@ public class IterateTest {
                 .collect(Collectors.toList());
 
         // then
-        assertTrue("should be 1", result.get(0) == 1);
-        assertTrue("should be 2", result.get(1) == 2);
-        assertTrue("should be 4", result.get(2) == 4);
-        assertTrue("should be 8", result.get(3) == 8);
-        assertTrue("should be 16", result.get(4) == 16);
+        assertThat(result).containsExactly(1,2,4,8,16);
 
     }
 }

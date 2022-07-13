38ee1a8a561d56b6e1b1c20981427136557dac70
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-07-13 11:31:44 -0300

    java 11 Predicate

--- src/test/java/com/rodtech/javaalgteststips/javaalgteststips/string/StringTest.java
+++ src/test/java/com/rodtech/javaalgteststips/javaalgteststips/string/StringTest.java
@@ -2,6 +2,7 @@ package com.rodtech.javaalgteststips.javaalgteststips.string;
 
 import org.junit.jupiter.api.Test;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,6 +18,18 @@ public class StringTest {
                 .map(String::strip)
                 .collect(Collectors.toList());
         // then
-        assertThat(lines).containsExactly("i'm", "your", "father. ");
+        assertThat(lines).containsExactly("I'm", "your", "father.");
+    }
+
+    @Test
+    public void shouldReturnLinesOfString2() {
+        // given
+        String multilineString = "I'm \n \n your \n father.";
+        List<String> lines = multilineString.lines()
+                .filter(Predicate.not(String::isBlank))
+                .map(String::strip)
+                .collect(Collectors.toList());
+        // then
+        assertThat(lines).containsExactly("I'm", "your", "father.");
     }
 }

165d72e1a5ca2810438c7461b2d2a17dd0b094ca
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-07-13 14:11:44 -0300

    using likedHashMap

--- src/test/java/com/rodtech/javaalgteststips/javaalgteststips/stream/groupingby/GroupingByTest.java
+++ src/test/java/com/rodtech/javaalgteststips/javaalgteststips/stream/groupingby/GroupingByTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -27,9 +29,17 @@ public class GroupingByTest {
                         )
                 );
 
-        assertEquals("Papaya should 1", result.get("papaya"), 1L);
-        assertEquals("Apple should 3", result.get("apple"), 3L);
-        assertEquals("Banana should 2", result.get("banana"), 2L);
-        assertEquals("Orage should 1", result.get("orange"), 1L);
+        //linked hashmap to keep ordination
+        var finalMap = new LinkedHashMap<>();
+
+        //Sort a map and add to finalMap
+        result.entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .forEachOrdered(e -> finalMap.put(e.getKey(), e.getValue()));
+
+        assertEquals("Papaya should 1", finalMap.get("papaya"), 1L);
+        assertEquals("Apple should 3", finalMap.get("apple"), 3L);
+        assertEquals("Banana should 2", finalMap.get("banana"), 2L);
+        assertEquals("Orage should 1", finalMap.get("orange"), 1L);
     }
 }
